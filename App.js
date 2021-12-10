a425d17ea84ff301a4a59da4c3b8d6c8ac756204
saadswe2020 <saadswe2019@gmail.com>
2021-12-10 14:12:08 +0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Provider } from 'react-native-paper'
+
+import { theme } from './src/core/theme'
+import LoginScreen  from './src/screens/LoginScreen';
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider theme={theme}>
+  
+          <LoginScreen></LoginScreen>
+    
+    </Provider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
